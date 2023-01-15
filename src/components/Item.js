7fe2cd22789647e7cd0c8654b35276f55aa46988
Tate <tatebraeckel@gmail.com>
2023-01-14 18:27:35 -0700

    commit tree 6d25bc31f889f2fc53f063c993a8c48a7d2ef50b

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 function Item({ item }) {
+
+  function handleAddToCart() {
+
+    fetch('http://localhost:4000/items/${item.id}', {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => console.log(updatedItem))
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button
+        onClick={handleAddToCart}
+        className={item.isInCart ? "remove" : "add"}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
